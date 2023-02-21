303c81ed6445e35880b2ead86f2656f8d0fe5581
EMIR1HUB <emir-23.03@mail.ru>
2023-02-21 14:21:54 +0300

    the result program

--- src/main/java/com/suleimanov/libraryproject/controllers/BookController.java
+++ src/main/java/com/suleimanov/libraryproject/controllers/BookController.java
@@ -7,20 +7,27 @@ import com.suleimanov.libraryproject.models.BookInfo;
 import com.suleimanov.libraryproject.models.PersonInfo;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/books")
 public class BookController {
     private final BookDAO bookDAO;
     private final PersonDAO personDAO;
-
     private final BookPhotoDAO bookPhotoDAO;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @Autowired
     public BookController(BookDAO bookDAO, PersonDAO personDAO, BookPhotoDAO bookPhotoDAO) {
@@ -30,19 +37,19 @@ public class BookController {
     }
 
     @GetMapping()
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("books", bookDAO.index());
         return "books/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Long id, Model model,
-                       @ModelAttribute("person") PersonInfo person){
+                       @ModelAttribute("person") PersonInfo person) {
         model.addAttribute("book", bookDAO.show(id));
         model.addAttribute("bookPatch", bookPhotoDAO.showPath(id));
 
         Optional<PersonInfo> bookOwner = bookDAO.getBookOwner(id);
-        if(bookOwner.isPresent())
+        if (bookOwner.isPresent())
             model.addAttribute("owner", bookOwner.get());
         else
             model.addAttribute("people", personDAO.index());
@@ -55,10 +62,24 @@ public class BookController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("book") @Valid BookInfo bookInfo, BindingResult bindingResult) {
+    public String create(@ModelAttribute("book") @Valid BookInfo bookInfo,
+                         BindingResult bindingResult, @RequestParam("file") MultipartFile file) throws IOException {
 
         if (bindingResult.hasErrors()) return "books/new";
         bookDAO.save(bookInfo);
+
+        // если фото было отправленно, то сохраняем его в директории
+        if(!file.isEmpty()){
+            File uploadDir = new File(uploadPath + "/books");
+            if(!uploadDir.exists()) // если пути не существует
+                uploadDir.mkdir();
+
+            String resultFileName = UUID.randomUUID().toString() + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/books/" + resultFileName));
+
+            bookPhotoDAO.save(resultFileName);
+        }
+
         return "redirect:/books";
     }
 
@@ -70,27 +91,45 @@ public class BookController {
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("book") @Valid BookInfo bookInfo,
-                         BindingResult bindingResult, @PathVariable("id") Long id) {
+                         BindingResult bindingResult, @PathVariable("id") Long id,
+                         @RequestParam("file") MultipartFile file) throws IOException {
         if (bindingResult.hasErrors()) return "books/edit";
-
         bookDAO.update(id, bookInfo);
+
+        System.out.println("file " + file.getOriginalFilename());
+        if(!file.isEmpty()){
+            File uploadDir = new File(uploadPath + "/books");
+            if(!uploadDir.exists())     // если пути не существует
+                uploadDir.mkdir();
+
+            String resultFileName = UUID.randomUUID().toString() + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/books/" + resultFileName));
+
+            if (bookPhotoDAO.showPath(id).isPresent())  // удаляем из директории старый файл
+                new File(uploadPath + "/books/" + bookPhotoDAO.showPath(id).get().getPathFileName()).delete();
+            if(bookPhotoDAO.showPath(id).isEmpty())     // если нужно установить фото
+                bookPhotoDAO.save(id, resultFileName);
+            if(bookPhotoDAO.showPath(id).isPresent())   // если нужно обновить старое фото
+                bookPhotoDAO.update(id, resultFileName);
+        }
         return "redirect:/books";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") Long id) {
+        new File(uploadPath + "/books/" + bookPhotoDAO.showPath(id).get().getPathFileName()).delete();
         bookDAO.delete(id);
         return "redirect:/books";
     }
 
     @PatchMapping("/{id}/assign")
-    public String assign(@PathVariable("id") Long id, @ModelAttribute("person") PersonInfo personInfo){
+    public String assign(@PathVariable("id") Long id, @ModelAttribute("person") PersonInfo personInfo) {
         bookDAO.assign(id, personInfo);
         return "redirect:/books/" + id;
     }
 
     @PatchMapping("/{id}/release")
-    public String release (@PathVariable("id") Long id){
+    public String release(@PathVariable("id") Long id) {
         bookDAO.release(id);
         return "redirect:/books/" + id;
     }
--- src/main/java/com/suleimanov/libraryproject/dao/BookPhotoDAO.java
+++ src/main/java/com/suleimanov/libraryproject/dao/BookPhotoDAO.java
@@ -2,6 +2,7 @@ package com.suleimanov.libraryproject.dao;
 
 import com.suleimanov.libraryproject.models.BookInfo;
 import com.suleimanov.libraryproject.models.BookPhotoInfo;
+import com.suleimanov.libraryproject.models.PersonInfo;
 import com.suleimanov.libraryproject.models.PersonPhotoInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -20,6 +21,21 @@ public class BookPhotoDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    public void save(Long id, String fileName){
+        String SQL = "INSERT INTO book_photo(book_id, path_file_name) VALUES (?, ?)";
+        jdbcTemplate.update(SQL, id, fileName);
+    }
+
+    public void save(String fileName){
+        String SQL = "INSERT INTO book_photo(book_id, path_file_name) VALUES (?, ?)";
+        jdbcTemplate.update(SQL, getLastId().getId(), fileName);
+    }
+
+    public PersonInfo getLastId(){
+        return jdbcTemplate.query("SELECT id FROM book ORDER BY id DESC LIMIT 1;",
+                new BeanPropertyRowMapper<>(PersonInfo.class)).stream().findAny().orElse(null);
+    }
+
     public Optional<BookPhotoInfo> showPath(Long id){
         String SQL = "SELECT path_file_name FROM book_photo WHERE book_id = ?";
         return jdbcTemplate.query(SQL, new Object[]{id}, new BeanPropertyRowMapper<>(BookPhotoInfo.class)).stream().findAny();
@@ -27,5 +43,10 @@ public class BookPhotoDAO {
 
     public List<BookPhotoInfo> index() {
         return jdbcTemplate.query("SELECT * FROM book_photo", new BeanPropertyRowMapper<>(BookPhotoInfo.class));
+    }
+
+    public void update(Long id, String fileName){
+        String SQL = "UPDATE book_photo SET path_file_name = ? WHERE book_id = ?";
+        jdbcTemplate.update(SQL, fileName, id);
     }
 }
